61979f08e224ecf9a86e7ed541fe76091b9131f4
Bartosz Piechnik <bartoszpiechnik25@gmail.com>
2024-03-25 14:59:10 +0100

    Add reflection to support grpcurl

--- etf_service/main.py
+++ etf_service/main.py
@@ -2,8 +2,10 @@ import grpc
 import asyncio
 
 from v1.etf_service_pb2_grpc import add_EtfServiceServicer_to_server
+from v1 import etf_service_pb2
 from v1.service import EtfServiceServicer
 from config import get_db, get_config
+from grpc_reflection.v1alpha import reflection
 
 CONFIG = get_config()
 DB = get_db(CONFIG)
@@ -14,6 +16,11 @@ async def serve():
     add_EtfServiceServicer_to_server(
         EtfServiceServicer(DB, CONFIG["ETF_COLLECTION"]), server
     )
+    SERVICE_NAMES = (
+        etf_service_pb2.DESCRIPTOR.services_by_name["EtfService"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port("[::]:50051")
     await server.start()
     await server.wait_for_termination()
