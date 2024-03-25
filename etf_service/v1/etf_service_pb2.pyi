65609b9748e421f1c83b7611b65132c9fec44834
Bartosz Piechnik <bartoszpiechnik25@gmail.com>
2024-03-25 21:57:07 +0100

    working version of GetEtf, AddEtf and GetEtfPerformance tested with grpcurl

--- etf_service/v1/etf_service_pb2.pyi
+++ etf_service/v1/etf_service_pb2.pyi
@@ -1,13 +1,7 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import (
-    ClassVar as _ClassVar,
-    Iterable as _Iterable,
-    Mapping as _Mapping,
-    Optional as _Optional,
-    Union as _Union,
-)
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -19,12 +13,7 @@ class EtfHoldings(_message.Message):
     symbol: str
     holding_name: str
     holding_percent: float
-    def __init__(
-        self,
-        symbol: _Optional[str] = ...,
-        holding_name: _Optional[str] = ...,
-        holding_percent: _Optional[float] = ...,
-    ) -> None: ...
+    def __init__(self, symbol: _Optional[str] = ..., holding_name: _Optional[str] = ..., holding_percent: _Optional[float] = ...) -> None: ...
 
 class SectorWeights(_message.Message):
     __slots__ = ("sector_name", "sector_weight")
@@ -32,21 +21,10 @@ class SectorWeights(_message.Message):
     SECTOR_WEIGHT_FIELD_NUMBER: _ClassVar[int]
     sector_name: str
     sector_weight: float
-    def __init__(
-        self, sector_name: _Optional[str] = ..., sector_weight: _Optional[float] = ...
-    ) -> None: ...
+    def __init__(self, sector_name: _Optional[str] = ..., sector_weight: _Optional[float] = ...) -> None: ...
 
 class Etf(_message.Message):
-    __slots__ = (
-        "yahoo_symbol",
-        "name",
-        "currency",
-        "holdings",
-        "weights",
-        "total_assets",
-        "expense_ratio",
-        "last_close",
-    )
+    __slots__ = ("yahoo_symbol", "name", "currency", "holdings", "weights", "total_assets", "expense_ratio", "last_close")
     YAHOO_SYMBOL_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     CURRENCY_FIELD_NUMBER: _ClassVar[int]
@@ -63,17 +41,29 @@ class Etf(_message.Message):
     total_assets: int
     expense_ratio: float
     last_close: float
-    def __init__(
-        self,
-        yahoo_symbol: _Optional[str] = ...,
-        name: _Optional[str] = ...,
-        currency: _Optional[str] = ...,
-        holdings: _Optional[_Iterable[_Union[EtfHoldings, _Mapping]]] = ...,
-        weights: _Optional[_Iterable[_Union[SectorWeights, _Mapping]]] = ...,
-        total_assets: _Optional[int] = ...,
-        expense_ratio: _Optional[float] = ...,
-        last_close: _Optional[float] = ...,
-    ) -> None: ...
+    def __init__(self, yahoo_symbol: _Optional[str] = ..., name: _Optional[str] = ..., currency: _Optional[str] = ..., holdings: _Optional[_Iterable[_Union[EtfHoldings, _Mapping]]] = ..., weights: _Optional[_Iterable[_Union[SectorWeights, _Mapping]]] = ..., total_assets: _Optional[int] = ..., expense_ratio: _Optional[float] = ..., last_close: _Optional[float] = ...) -> None: ...
+
+class EtfPerformance(_message.Message):
+    __slots__ = ("ytd", "one_month", "three_months", "six_months", "one_year", "three_years", "five_years", "max", "last_close")
+    YTD_FIELD_NUMBER: _ClassVar[int]
+    ONE_MONTH_FIELD_NUMBER: _ClassVar[int]
+    THREE_MONTHS_FIELD_NUMBER: _ClassVar[int]
+    SIX_MONTHS_FIELD_NUMBER: _ClassVar[int]
+    ONE_YEAR_FIELD_NUMBER: _ClassVar[int]
+    THREE_YEARS_FIELD_NUMBER: _ClassVar[int]
+    FIVE_YEARS_FIELD_NUMBER: _ClassVar[int]
+    MAX_FIELD_NUMBER: _ClassVar[int]
+    LAST_CLOSE_FIELD_NUMBER: _ClassVar[int]
+    ytd: float
+    one_month: float
+    three_months: float
+    six_months: float
+    one_year: float
+    three_years: float
+    five_years: float
+    max: float
+    last_close: float
+    def __init__(self, ytd: _Optional[float] = ..., one_month: _Optional[float] = ..., three_months: _Optional[float] = ..., six_months: _Optional[float] = ..., one_year: _Optional[float] = ..., three_years: _Optional[float] = ..., five_years: _Optional[float] = ..., max: _Optional[float] = ..., last_close: _Optional[float] = ...) -> None: ...
 
 class GetEtfByName(_message.Message):
     __slots__ = ("name",)
--- etf_service/v1/service.py
+++ etf_service/v1/service.py
@@ -1,13 +1,21 @@
 import v1.etf_service_pb2 as etf_service_pb3
 import v1.etf_service_pb2_grpc as etf_service_pb3_grpc
+from typing import Tuple, LiteralString
 import yahooquery as yq
 import yfinance as yf
 import asyncio
+import grpc
+
+
+def ticker_exists(ticker: yf.Ticker) -> dict | None:
+    return ticker.get_info() if len(ticker.history()) > 0 else None
 
 
 async def get_specifics(ticker: str, data: dict, message: dict):
-    etf_specifics = yq.Ticker(ticker)
-    holding_info = etf_specifics.fund_holding_info[ticker]
+    etf_specifics = yq.Ticker(ticker, asynchronus=True)
+    holding_info = etf_specifics.fund_holding_info.get(ticker, None)
+    if holding_info is None:
+        return None
 
     message["holdings"], data["holdings"] = [], []
     for holding in holding_info["holdings"]:
@@ -40,36 +48,106 @@ async def get_specifics(ticker: str, data: dict, message: dict):
             )
     data["expense_ratio"] = etf_specifics.fund_profile[ticker][
         "feesExpensesInvestment"
-    ]["annualReportExpenseRatio"]
+    ].get("annualReportExpenseRatio", 0)
 
 
 class EtfServiceServicer(etf_service_pb3_grpc.EtfServiceServicer):
+    periods: Tuple[Tuple[LiteralString, LiteralString]] = (
+        ("ytd", "ytd"),
+        ("1mo", "one_month"),
+        ("3mo", "three_months"),
+        ("6mo", "six_months"),
+        ("1y", "one_year"),
+        ("3y", "three_years"),
+        ("5y", "five_years"),
+        ("max", "max"),
+    )
+
     def __init__(self, db_collection, collection_name) -> None:
         super().__init__()
         self.db = db_collection[collection_name]
 
-    async def GetEtf(self, request, context):
+    async def GetEtf(self, request, context: grpc.ServicerContext):
         res = self.db.find_one({"yahoo_symbol": request.yahoo_symbol}, {"_id": False})
+        if res is None:
+            await context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"ETF with ticker: {request.yahoo_symbol} not found",
+            )
         return etf_service_pb3.Etf(**res)
 
-    async def AddEtf(self, request, context):
+    async def AddEtf(self, request, context: grpc.ServicerContext):
+        if self.db.find_one({"yahoo_symbol": request.yahoo_symbol}) is not None:
+            await context.abort(
+                grpc.StatusCode.ALREADY_EXISTS,
+                f"ETF with ticker: {request.yahoo_symbol} already exists in the db",
+            )
+
+        etf = yf.Ticker(request.yahoo_symbol)
+        etf_info = ticker_exists(etf)
+        if etf_info is None:
+            await context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"ETF with ticker: {request.yahoo_symbol} not found",
+            )
+
         data, spec, mesage = {}, {}, {}
         specifics_task = asyncio.create_task(
             get_specifics(request.yahoo_symbol, spec, mesage)
         )
 
-        etf = yf.Ticker(request.yahoo_symbol)
-        etf_info = etf.get_info()
         data["name"] = etf_info["longName"]
         data["currency"] = etf_info["currency"]
         data["yahoo_symbol"] = etf_info["symbol"]
         data["last_close"] = etf_info["previousClose"]
-        data["total_assets"] = 0
+        data["total_assets"] = etf_info.get("totalAssets", 0)
 
-        await specifics_task
+        details = await specifics_task
+        if details is None:
+            await context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"cannot fetch ETF details for: {request.yahoo_symbol}",
+            )
 
         etf_db_data = {**data, **spec}
         etf_message = {**mesage, **spec}
         self.db.insert_one(etf_db_data)
 
         return etf_service_pb3.Etf(**etf_message)
+
+    async def GetEtfPerformance(self, request, context: grpc.ServicerContext):
+        etf = yf.Ticker(request.yahoo_symbol)
+
+        if ticker_exists(etf) is None:
+            await context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"cannot resolve symbol: {request.yahoo_symbol}",
+            )
+
+        last_close = float(etf.history(period="1d").iloc[-1]["Close"])
+
+        update_db = asyncio.create_task(
+            self.update_price(request.yahoo_symbol, last_close)
+        )
+
+        prices = {
+            message_field_name: float(etf.history(period=per).iloc[0]["Close"])
+            for per, message_field_name in self.periods
+        }
+        prices["last_close"] = last_close
+
+        if await update_db is None:
+            await context.abort(
+                grpc.StatusCode.NOT_FOUND,
+                f"could not update ETF with ticker: {request.yahoo_symbol}",
+            )
+
+        return etf_service_pb3.EtfPerformance(**prices)
+
+    async def update_price(self, ticker: str, value: float):
+        try:
+            return self.db.find_one_and_update(
+                {"yahoo_symbol": ticker}, {"$set": {"last_close": value}}
+            )
+        except Exception:
+            return None
